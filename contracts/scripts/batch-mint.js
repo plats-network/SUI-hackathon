a6d297f64fa6e69d4d48fda057d37c5d98371eaa
CocDap <48429826+CocDap@users.noreply.github.com>
2024-04-17 16:13:55 +0700

    chore: update script for mint batch (#31)

--- contracts/scripts/batch-mint.js
+++ contracts/scripts/batch-mint.js
@@ -1,5 +1,5 @@
-const {TransactionBlock}  = require("@mysten/sui.js/transactions");
-const {Ed25519Keypair} = require("@mysten/sui.js/keypairs/ed25519");
+const { TransactionBlock } = require("@mysten/sui.js/transactions");
+const { Ed25519Keypair } = require("@mysten/sui.js/keypairs/ed25519");
 const { getFullnodeUrl, SuiClient } = require('@mysten/sui.js/client');
 const dotenv = require('dotenv');
 dotenv.config();
@@ -7,17 +7,18 @@ dotenv.config();
 if (!process.env.PACKAGE_ID) {
     console.log('Requires PACKAGE_ID; set with `export PACKAGE_ID="..."`');
     process.exit(1);
-  }
+}
 
 async function mint() {
     const keypair = Ed25519Keypair.deriveKeypair(process.env.MNEMONIC_CLIENT);
     const client = new SuiClient({
         url: getFullnodeUrl('testnet'),
     });
-    const tx = new TransactionBlock();
-    //let packageId = "0x769941cd7b338429e9ada6f6e697e47461971c6bc2c8c45d8a1f3e412c4767ea";
+
     let packageId = process.env.PACKAGE_ID;
     let collectionId = process.env.COLLECTION_ID;
+    let txs = new Array();
+    const tx = new TransactionBlock();
     tx.moveCall({
         target: `${packageId}::client::mint_batch`,
         arguments: [
@@ -33,25 +34,62 @@ async function mint() {
             // catogory: vector<u8>,
             tx.pure("Standard"),
             // max_supply: u64,
-            tx.pure(2),
+            tx.pure(1),
+        ],
+        typeArguments: [`${packageId}::ticket_collection::NFTTicket`]
+    });
+    txs.push(tx);
+    const tx2 = new TransactionBlock();
+    tx2.moveCall({
+        target: `${packageId}::client::mint_batch`,
+        arguments: [
+            // collection object id 
+            //tx.object('0x0d6422b82f418e592546019b81585963300f2f29acb86a281e5add34f3388c7d'),
+            tx2.object(collectionId),
+            // name: vector<u8>,
+            tx2.pure("SUI Hackathon 1"),
+            // description: vector<u8>,
+            tx2.pure("This is a ticket VIP yo join SUI Hackathon"),
+            // url: vector<u8>,
+            tx2.pure("https://picsum.photos/id/237/200/300"),
+            // catogory: vector<u8>,
+            tx2.pure("VIP"),
+            // max_supply: u64,
+            tx2.pure(1),
         ],
         typeArguments: [`${packageId}::ticket_collection::NFTTicket`]
     });
 
-    const result = await client.signAndExecuteTransactionBlock({
-        signer: keypair,
-        transactionBlock: tx,
-    });
+    txs.push(tx2);
 
-    console.log({ result });
+    for (const tx of txs) {
+        const result = await client.signAndExecuteTransactionBlock({
+            signer: keypair,
+            transactionBlock: tx,
+            options: {
+                showObjectChanges: true,
+            },
+            //requestTypes:'WaitForEffectsCert'
+        });
 
-    const ticketIds = 
-    result.objectChanges.filter(
-        (o) =>
-            o.type === "created" &&
-            o.objectType.includes("::ticket_collection::NFTTicket")
-    ).map(item => item.objectId);
-    console.log(`ticket id : ${ticketIds}`);
+        console.log({ result });
+        const ticketIds =
+            result.objectChanges.filter(
+                (o) =>
+                    o.type === "created" &&
+                    o.objectType.includes("::ticket_collection::NFTTicket")
+            ).map(item => item.objectId);
+        console.log(`ticket id : ${ticketIds}`);
+        await sleep(2000);
+    }
+
+
+
+
+}
+
+async function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 mint();
